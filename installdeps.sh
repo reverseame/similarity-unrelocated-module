ff2590a53e681df297766858fd0098b5f99152d3
Miguel Martin-Perez <miguelmartinperez@unizar.es>
2021-11-11 18:29:37 +0100

    Installation fixed

--- installdeps.sh
+++ installdeps.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 # Dependencies
-systemdeps="python2.7 python2.7-dev python-pip ssdeep libfuzzy-dev git libffi-dev libssl1.0.0 build-essential"
-pythondeps="pycrypto distorm3 pefile ssdeep fuzzyhashlib capstone"
+systemdeps="python python-dev python-pip git multiarch-support build-essential libffi-dev libfuzzy-dev"
 
+pythondeps="pycrypto distorm3 pefile==2019.4.18 ssdeep fuzzyhashlib capstone"
 
 # Install system dependencies
 apt-get install -y $systemdeps
+
+# Install libcrypto.so.1.0.0 from a official repository
+wget https://deb.debian.org/debian/pool/main/o/openssl/libssl1.0.0_1.0.1t-1+deb8u8_amd64.deb
+dpkg -i libssl1.0.0_1.0.1t-1+deb8u8_amd64.deb
+rm libssl1.0.0_1.0.1t-1+deb8u8_amd64.deb
+
+# Update setuptools
+pip install --upgrade setuptools
 
 # Install python dependencies
 pip2 install $pythondeps
 
 # Marked Pefile
-sudo -u $SUDO_USER git clone git@github.com:miguelmartinperez/markedPefile.git marked_pefile
-cd marked_pefile
-sudo -u $SUDO_USER sh installdeps.sh
-cd ..
+if [ ! -z 'ls marked_pefile' ]; then
+    sudo -u $SUDO_USER git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/miguelmartinperez/markedPefile.git marked_pefile
+fi
 
