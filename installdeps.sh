16c0948e939d867d03846f218c19c58a457bb587
Miguel Martin-Perez <miguelmartinperez@unizar.es>
2021-12-16 12:02:13 +0100

    Fixed submodule clone 3

--- installdeps.sh
+++ installdeps.sh
@@ -20,7 +20,7 @@ pip install --upgrade setuptools
 pip2 install $pythondeps
 
 # Marked Pefile
-if [ "$(ls -A marked_pefile)" ]; then
+if [ ! "$(ls -A marked_pefile)" ]; then
     sudo -u $SUDO_USER git clone --depth 1 --recurse-submodules --shallow-submodules https://github.com/miguelmartinperez/markedPefile.git marked_pefile
 fi
 
